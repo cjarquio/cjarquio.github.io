2438146285f3ff8985729b3171c3439af96a0df6
Charles Jarquio <cjarquio@gmail.com>
2025-04-09 00:32:03 -0700

    Change Main to typescript file

---
+++ src/components/MainComponent.tsx
@@ -0,0 +1,15 @@
+import Home from './HomeComponent';
+import Header from './AppWrapper/HeaderComponent';
+import Footer from './AppWrapper/FooterComponent';
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
