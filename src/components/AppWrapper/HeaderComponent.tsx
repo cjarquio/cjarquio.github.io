eb89d241414e568d514daf252d33c50a365859df
Charles Jarquio <cjarquio@gmail.com>
2025-04-09 00:30:40 -0700

    Change HeaderComponent to typescript file

---
+++ src/components/AppWrapper/HeaderComponent.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Navbar, Nav, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar dark expand="md">
+        <div className="container homeText">
+          <Nav navbar>
+            <NavItem>
+              <NavLink className="nav-link" to="/home">
+                <span className="fa fa-home fa-lg"></span> Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className="nav-link" to="/projects">
+                <span className="fa fa-code fa-lg"></span> Projects
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className="nav-link" to="/resume">
+                <span className="fa fa-file fa-lg"></span> Resume
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </div>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
