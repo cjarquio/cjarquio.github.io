9cadaeadfcf597f380030d4578a8f4cf7578f7d1
Charles Jarquio <cjarquio@gmail.com>
2025-04-10 12:06:30 -0700

    Set up mantine

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@mantine/core/styles.css';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import PortfolioRouter from './components/PortfolioRouter/PortfolioRouter';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <PortfolioRouter />
-    </BrowserRouter>
+    <MantineProvider theme={theme}>
+      <BrowserRouter>
+        <PortfolioRouter />
+      </BrowserRouter>
+    </MantineProvider>
   </StrictMode>
 );
