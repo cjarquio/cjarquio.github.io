f411e1dd4c51ba38b54aac00de36ba105a59c2ef
Charles Jarquio <cjarquio@gmail.com>
2025-04-24 20:43:15 -0700

    Add component for about me

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,10 @@
 import { Container, Flex, Group, Tabs } from '@mantine/core';
+import { AboutMe } from './AboutMe/AboutMe';
 
 export const About = () => {
   return (
     <Container fluid>
-      <Group align="center">
+      <Group grow preventGrowOverflow={false} wrap="nowrap">
         {/** TODO: Insert image */}
         <h1>About Me</h1>
         <Flex gap={5} justify="start" align="start">
@@ -16,7 +17,9 @@ export const About = () => {
             </Tabs.List>
 
             {/** TODO: Create separate components for each section */}
-            <Tabs.Panel value="about">Gallery tab content</Tabs.Panel>
+            <Tabs.Panel value="about">
+              <AboutMe />
+            </Tabs.Panel>
             <Tabs.Panel value="experience">Messages tab content</Tabs.Panel>
             <Tabs.Panel value="education">Education</Tabs.Panel>
             <Tabs.Panel value="skills">Skills</Tabs.Panel>
---
+++ src/components/About/AboutMe/AboutMe.tsx
@@ -0,0 +1,23 @@
+import { Container, Text } from '@mantine/core';
+
+export const AboutMe = () => {
+  return (
+    <Container fluid>
+      <Text>
+        I am a software engineer with a passion for building web applications. I
+        have experience in various technologies and frameworks, including React,
+        Node.js, and Python. I enjoy solving complex problems and continuously
+        learning new skills to improve my craft.
+      </Text>
+      <Text>
+        I am an aspiring Software Engineer from the state of Hawai'i. I
+        graduated from the University of Hawaii at Manoa with a degree in
+        Computer Engineering. I am a fast learner with excellent time management
+        who's always eager to learn more; which is why I decided to attend the
+        Nucamp Web Developer boot camp to continue my education. Through a
+        combination of these courses, I have gained a strong work ethic in
+        individual and team based settings.
+      </Text>
+    </Container>
+  );
+};
