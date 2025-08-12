f8f716a958030c6e81d6ab30021db6ab0a904f8b
Charles Jarquio <cjarquio@gmail.com>
2025-08-12 15:36:45 -0700

    Fix stylings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,28 @@
-'use client';
-import { mantineHtmlProps, MantineProvider } from '@mantine/core';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  ColorSchemeScript,
+  mantineHtmlProps,
+  MantineProvider,
+} from '@mantine/core';
 import theme from './theme';
+import './globals.css';
+import '@mantine/core/styles.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Charles Jarquio | Portfolio',
+  description: 'My Portfolio showcasing my projects and skills.',
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en" {...mantineHtmlProps}>
-      <head></head>
+      <head>
+        <ColorSchemeScript />
+      </head>
       <body className="antialiased">
         <MantineProvider theme={theme}>{children}</MantineProvider>
       </body>
