8e998258a3d0707f8c1f6b6dd1ae425db4523c82
Charles Jarquio <cjarquio@gmail.com>
2025-08-13 13:03:59 -0700

    Add route to contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Contact from '@/components/Contact/Contact';
+
+export const ContactPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Contact />
+    </div>
+  );
+};
+
+export default ContactPage;
