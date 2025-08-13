da0c091c243d456c11a7d1198ce5eb76c54eaded
Charles Jarquio <cjarquio@gmail.com>
2025-08-13 14:02:43 -0700

    Move main project page component to pages folder

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,21 @@
-import Projects from '@/components/Projects/Projects';
+import { Container, SimpleGrid } from '@mantine/core';
+import { ProjectCard } from '@/components/Projects/ProjectCard/ProjectCard';
+import { projects, ProjectProps } from '@/components/Projects/projectList';
 
 export const ProjectsPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Projects />
-    </div>
+    <Container className={'flex items-center justify-evenly w-full'} size="xl">
+      <SimpleGrid
+        mt={60}
+        cols={{ base: 1, sm: 2, md: 3 }}
+        spacing={{ base: 'xl', md: 50 }}
+        verticalSpacing={{ base: 'xl', md: 50 }}
+      >
+        {projects.map((project: ProjectProps) => (
+          <ProjectCard key={project.title} project={project} />
+        ))}
+      </SimpleGrid>
+    </Container>
   );
 };
 
