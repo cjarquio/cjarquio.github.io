08290a822ba93c8a0f60d50a452fed5f4ec4c67e
Charles Jarquio <cjarquio@gmail.com>
2025-08-13 12:42:09 -0700

    Fixed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
 import {
+  AppShell,
+  AppShellFooter,
+  AppShellHeader,
+  AppShellMain,
   ColorSchemeScript,
   mantineHtmlProps,
   MantineProvider,
@@ -7,6 +11,8 @@ import theme from './theme';
 import './globals.css';
 import '@mantine/core/styles.css';
 import { Metadata } from 'next';
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
 
 export const metadata: Metadata = {
   title: 'Charles Jarquio | Portfolio',
@@ -24,7 +30,21 @@ export default function RootLayout({
         <ColorSchemeScript />
       </head>
       <body className="antialiased">
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <MantineProvider theme={theme}>
+          <AppShell
+            layout="alt"
+            header={{ height: '4rem' }}
+            footer={{ height: '6rem' }}
+          >
+            <AppShellHeader>
+              <Header />
+            </AppShellHeader>
+            <AppShellMain>{children}</AppShellMain>
+            <AppShellFooter p="lg">
+              <Footer />
+            </AppShellFooter>
+          </AppShell>
+        </MantineProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,3 @@
-import {
-  AppShell,
-  AppShellHeader,
-  AppShellMain,
-  AppShellFooter,
-} from '@mantine/core';
-import Footer from '../components/Footer/Footer';
-import Header from '../components/Header/Header';
-
 export default function Page() {
-  return (
-    <AppShell
-      layout="alt"
-      header={{ height: '4rem' }}
-      footer={{ height: '6rem' }}
-    >
-      <AppShellHeader>
-        <Header />
-      </AppShellHeader>
-      <AppShellMain>
-        <>HI</>
-      </AppShellMain>
-      <AppShellFooter p="lg">
-        <Footer />
-      </AppShellFooter>
-    </AppShell>
-  );
+  return <>HI</>;
 }
